a80463b4a27f47a30b32822d308d6c9b84f32dda
carlossau <106499782+carlossau@users.noreply.github.com>
2022-05-29 21:11:09 -0500

    pagina2

---
+++ pagina/scriptgaleria.js
@@ -0,0 +1,16 @@
+const fulImgBox= document.getElementById("fulimgBox");
+
+fulImg= document.getElementById("fulimg");
+
+funcion openFulImg(reference){
+fulimgBox.style.display="flex"
+fulImg.src = reference
+}
+
+
+
+
+funcion closeImg(){
+fulimgBox.style.display = "none";
+
+}
